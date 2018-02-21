1222dd67e393be1f9055781dc71eb963fc140eed
artmartinez <arturo.martinez@beeva.com>
2018-02-21 19:03:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Arturo Martinez',
     author_email='wankes2000@gmail.com',
     url='https://github.com/wankes2000/xCloudPy',  # use the URL to the github repo
-    download_url='https://github.com/wankes2000/xCloudPy/archive/0.2.0.tar.gz',
+    download_url='https://github.com/wankes2000/xCloudPy/archive/0.2.1.tar.gz',
     install_requires=['boto3==1.5.22', 'google-cloud-datastore==1.4.0', 'google-cloud-storage==1.7.0'],
     tests_require=['mock','nose==1.3.7','coverage==4.0.3', 'localstack==0.8.5'],
     keywords=['aws', 'cloud', 'google', 'gcp'],  # arbitrary keywords
